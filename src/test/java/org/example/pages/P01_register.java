1faafc32af7f8b9e4cf238750626cffe47145817
yasmin5794 <yasminabdelaziz66@gmail.com>
2023-02-23 03:42:09 +0200

    update register

--- src/test/java/org/example/pages/P01_register.java
+++ src/test/java/org/example/pages/P01_register.java
@@ -4,10 +4,44 @@ import org.example.stepDefs.Hooks;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 public class P01_register {
+    WebElement element = new WebDriverWait(Hooks.driver, Duration.ofSeconds(40))
+            .until(ExpectedConditions.presenceOfElementLocated(By.id("gender-female")));
     public WebElement registerLink()
+    { return Hooks.driver.findElement(By.className("ico-register")); }
+
+    public WebElement femaleGender()
+    { return Hooks.driver.findElement(By.id("gender-female")); }
+
+    public WebElement firstName(String FirstName) {
+         Hooks.driver.findElement(By.id("FirstName")).sendKeys(FirstName);
+           return Hooks.driver.findElement(By.id("FirstName")); }
+
+    public WebElement lastName(String LastName){
+         Hooks.driver.findElement(By.id("LastName")).sendKeys(LastName);
+           return Hooks.driver.findElement(By.id("LastName"));}
+
+    public WebElement enterDay(int Day)
+     {   Select select = new Select(Hooks.driver.findElement(By.name("DateOfBirthDay")));
+         select.selectByIndex(Day);
+           return Hooks.driver.findElement(By.name("DateOfBirthDay"));}
+    public WebElement enterMonth(int Month)
+     {   Select select = new Select(Hooks.driver.findElement(By.name("DateOfBirthMonth")));
+         select.selectByIndex(Month);
+           return Hooks.driver.findElement(By.name("DateOfBirthMonth")); }
+
+    public WebElement enterYear(int Year)
+     {  Select select = new Select(Hooks.driver.findElement(By.name("DateOfBirthYear")));
+        select.selectByIndex(Year);
+          return Hooks.driver.findElement(By.name("DateOfBirthYear")); }
+
+    public void enterEmail(String Email)
     {
-        return Hooks.driver.findElement(By.className("ico-register"));
+      Hooks.driver.findElement(By.id("Email")).sendKeys(Email);
     }
 }
--- src/test/java/org/example/stepDefs/D01_registerStepDef.java
+++ src/test/java/org/example/stepDefs/D01_registerStepDef.java
@@ -1,14 +1,34 @@
 package org.example.stepDefs;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import org.example.pages.P01_register;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class D01_registerStepDef {
-
     P01_register register = new P01_register();
+
     @Given("user go to register page")
     public void goRegisterPage()
-    {
-        register.registerLink().click();
-    }
+      { register.registerLink().click(); }
+
+      @And("user select gender type")
+      public void clickOnFemaleGender()
+         { register.femaleGender().click(); }
+
+      @And("user enter first name \"automation\" and last name \"tester\"")
+      public void enterFirstNameAndLastName()
+        {    register.firstName("automation");
+             register.lastName("tester");   }
+
+      @And("user enter date of birth")
+      public void enterDateOfBrith()
+        {  register.enterDay(5);
+           register.enterMonth(7);
+           register.enterYear(27);}
+
+      @And("user enter email \"test@example.com\" field")
+      public void userEnterEmail()
+      { register.enterEmail("test222@example.com"); }
 }
